671a48c42d214fc49962e198468fafd42333da44
Baba Koodu <adeshiname@gmail.com>
2022-01-09 11:34:28 +0100

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModels;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>{
+                new Movie
+                {
+                   Name="Futurama"
+                },
+                new Movie
+                {
+                   Name="Bojack Horseman"
+                }
+            };
+            var viewModel = new IndexMovieViewModel()
+            {
+                Movies = movies
+            };
+            return View(viewModel);
+        }
+    }
+}
