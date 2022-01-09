e3d07681c40ba016be382dd668e683af9066c3fb
Baba Koodu <adeshiname@gmail.com>
2022-01-09 11:56:06 +0100

    Refactor Customer to Database Query

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,21 +10,56 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer>
-            {
-                new Customer {
-                    Name="Adeshina"
-                }
-            };
+            var customers = _context.Customers.ToList();
+            //var customers = GetCustomers();
 
             var viewModel = new IndexCustomerViewModel
             {
                 Customers = customers
             };
             return View(viewModel);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customers = _context.Customers.ToList();
+            foreach (var customer in customers)
+            {
+                if(customer.Id == id)
+                {
+                    return View(customer);
+                }
+            }
+            return HttpNotFound();
+           
+        }
+
+        public List<Customer> GetCustomers() {
+            var customers = new List<Customer>
+            {
+                new Customer {
+                    Name="Adeshina"
+                },
+                new Customer {
+                    Name="Kelechukwu"
+                }
+            };
+            return customers;
         }
     }
 }
